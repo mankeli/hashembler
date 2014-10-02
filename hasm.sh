d7883f5cc3420bf64c525ca590facb7967e20579
Markus <markus@electrontu.be>
2014-10-02 23:31:41 +0300

    quotes are nice when working with paths

--- hasm.sh
+++ hasm.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -e
-HASMROOT=`dirname $0`
-HASMEXEC=/tmp/hasm
-g++ -o $HASMEXEC -std=c++11 "$@" -I$HASMROOT/src/
-$HASMEXEC
-rm $HASMEXEC
+HASMROOT="`dirname $0`"
+HASMEXEC="/tmp/hasm"
+g++ -o "$HASMEXEC" -std=c++11 "$@" -I"$HASMROOT/src/"
+"$HASMEXEC"
+rm "$HASMEXEC"
