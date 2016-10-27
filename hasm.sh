40a47d1d4d1a8c48eca1fa25e2007c5aab5f06bb
Markus Pyykkö <markus@electrontu.be>
2016-10-27 22:59:43 +0300

    hasm.sh now chooses unique filename for the executable

--- hasm.sh
+++ hasm.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 set -e
 HASMROOT="`dirname $0`"
-HASMEXEC="/tmp/hasm"
+HASMEXEC="/tmp/hasm$$"
 clang++ -g -o "$HASMEXEC" -std=c++11 "$@" -I"$HASMROOT/src/"
 "$HASMEXEC"
 echo "$HASMEXEC"
